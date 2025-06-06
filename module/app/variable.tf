2cdff745c26db9b5de14c5f2ae4e930df033a378
Pavani <vemasani.pavani@gmail.com>
2025-06-06 21:40:43 +0530

    terraform

--- module/app/variable.tf
+++ module/app/variable.tf
@@ -1,9 +1,9 @@
-variable "instance_type" {}
-variable "component" {}
+//variable "instance_type" {}
+//variable "component" {}
 variable "env" {}
-variable "ssh_user"{}
-variable "ssh_pass"{}
-variable "zone_id"{}
+//variable "ssh_user"{}
+//variable "ssh_pass"{}
+//variable "zone_id"{}
 
 output "env" {
   value = var.env
--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "env"{}
-variable "instance_type"{}
-variable "ssh_user"{}
-variable "ssh_pass"{}
-variable "component"{}
-variable "zone_id"{}
+//variable "instance_type"{}
+//variable "ssh_user"{}
+//variable "ssh_pass"{}
+//variable "component"{}
+//variable "zone_id"{}
 
 
 
