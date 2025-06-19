cce4680fd0795a01084de6f5a0e387c2f898f746
Your Name <vemasani.pavani@gmail.com>
2025-06-19 14:26:39 +0530

    peer connection between two vpc's

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -16,4 +16,8 @@ resource "aws_subnet" "subnet" {
 resource "aws_vpc_peering_connection" "foo" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.vpc.id
+  auto_accept   = true
+  tags = {
+  Name = "${var.env}-peer"
+  }
 }
