4e6bcc2389c0dee95c48835ae8deae0f2b92eab7
Your Name <vemasani.pavani@gmail.com>
2025-06-19 14:52:27 +0530

    edit routes both sides of vpc

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc_peering_connection" "peer" {
 }
 
 resource "aws_route" "main_edit_route" {
-  route_table_id            = aws_vpc.main_edit_route.route_table_id
+  route_table_id            = var.vpc_route_table_id
   destination_cidr_block    = var.destination_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 }
