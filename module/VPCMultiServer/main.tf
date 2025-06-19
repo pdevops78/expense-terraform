008a409d82e2b5163c947a55ee8dea87109f1ef0
Your Name <vemasani.pavani@gmail.com>
2025-06-19 15:48:39 +0530

    expose resource outside

--- module/VPCMultiServer/main.tf
+++ module/VPCMultiServer/main.tf
@@ -24,3 +24,21 @@ resource "aws_subnet" "frontend_subnets" {
     Name = "${var.env}-frontend-${count.index+1}"
   }
 }
+resource "aws_subnet" "backend_subnets" {
+  count       = length(var.frontendServers)
+  vpc_id      = aws_vpc.vpc.id
+  cidr_block  = var.frontendServers[count.index]
+  availability_zone = var.availability_zone[count.index]
+  tags = {
+    Name = "${var.env}-frontend-${count.index+1}"
+  }
+}
+resource "aws_subnet" "db_subnets" {
+  count       = length(var.frontendServers)
+  vpc_id      = aws_vpc.vpc.id
+  cidr_block  = var.frontendServers[count.index]
+  availability_zone = var.availability_zone[count.index]
+  tags = {
+    Name = "${var.env}-frontend-${count.index+1}"
+  }
+}
--- module/VPCMultiServer/output.tf
+++ module/VPCMultiServer/output.tf
@@ -1,3 +1,12 @@
 output "vpc"{
-vpc_id  = aws_vpc.vpc.id
+value  = aws_vpc.vpc.id
+}
+output "frontendServers"{
+value = aws_subnet.frontend_subnets.*.id
+}
+output "backendServers"{
+value = aws_subnet.backend_subnets.*.id
+}
+output "dbServers"{
+value = aws_subnet.db_subnets.*.id
 }
