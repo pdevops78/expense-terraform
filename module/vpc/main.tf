6e919767acba7df143a12e167560c3524513388e
Your Name <vemasani.pavani@gmail.com>
2025-06-19 14:56:40 +0530

    peer connection

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -22,14 +22,14 @@ resource "aws_vpc_peering_connection" "peer" {
   }
 }
 
-resource "aws_route" "main_edit_route" {
-  route_table_id            = var.vpc_route_table_id
-  destination_cidr_block    = var.default_vpc_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-
-resource "aws_route" "default_edit_route" {
-  route_table_id            = var.default_vpc_route_table_id
-  destination_cidr_block    = var.vpc_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+# resource "aws_route" "main_edit_route" {
+#   route_table_id            = var.vpc_route_table_id
+#   destination_cidr_block    = var.default_vpc_cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+# }
+#
+# resource "aws_route" "default_edit_route" {
+#   route_table_id            = var.default_vpc_route_table_id
+#   destination_cidr_block    = var.vpc_cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+# }
