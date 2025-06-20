2933958c494051940e8da35a4da9701148507df8
Your Name <vemasani.pavani@gmail.com>
2025-06-20 10:10:55 +0530

    comment

--- module/VPCMultiServer/output.tf
+++ module/VPCMultiServer/output.tf
@@ -4,9 +4,9 @@ value  = aws_vpc.vpc.id
 output "frontendServers"{
 value = aws_subnet.frontend_subnets.*.id
 }
-output "backendServers"{
-value = aws_subnet.backend_subnets.*.id
-}
-output "dbServers"{
-value = aws_subnet.db_subnets.*.id
-}
+# output "backendServers"{
+# value = aws_subnet.backend_subnets.*.id
+# }
+# output "dbServers"{
+# value = aws_subnet.db_subnets.*.id
+# }
