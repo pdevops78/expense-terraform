3785fab7223a503f76d5e227a20b79046c2ae772
Your Name <vemasani.pavani@gmail.com>
2025-06-22 06:36:53 +0530

    add  default vpc cidr block

--- module/VPC/VPCInternet/main.tf
+++ module/VPC/VPCInternet/main.tf
@@ -160,9 +160,8 @@ resource "aws_route_table" "public" {
 resource "aws_route" "public_route" {
   count                     = length(var.publicServers)
   route_table_id            = aws_route_table.public[count.index].id
-  destination_cidr_block    = "0.0.0.0/0"
+  destination_cidr_block    = var.default_vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  gateway_id                = aws_internet_gateway.igw.id
  }
 
 #  associate subnets with route table id
