f6fd9be27961e01ad926f161758e300a65ba423c
Your Name <vemasani.pavani@gmail.com>
2025-06-23 13:41:31 +0530

    security group is a list

--- module/app/main.tf
+++ module/app/main.tf
@@ -58,7 +58,7 @@ resource "null_resource" "provisioner" {
     }
     inline = [
       "sudo pip3.11 install ansible hvac",
-      "ansible-pull -i localhost, -U https://github.com/pdevops78/expense-ansible/getsecrets.yml -e env=${var.env} -e component_name=${var.component}",
+      "ansible-pull -i localhost, -U https://github.com/pdevops78/expense-ansible getsecrets.yml -e env=${var.env} -e component_name=${var.component}" -e project_name="expense",
       "ansible-pull -i localhost, -U https://github.com/pdevops78/expense-ansible expense.yml -e env=${var.env} -e component_name=${var.component} -e @secrets.json -e @app.json"
     ]
   }
