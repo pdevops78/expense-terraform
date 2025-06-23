562b0d2c37fb8072d64d937edf8b27dd83d8ea08
Your Name <vemasani.pavani@gmail.com>
2025-06-23 13:03:46 +0530

    additional braces added

--- variables.tf
+++ variables.tf
@@ -10,10 +10,10 @@ variable "default_vpc_id"{}
 variable "default_vpc_cidr_block"{}
 variable "default_vpc_route_table_id"{}
 variable "vpc_route_table_id"{}
-variable "frontendServers"{}
-variable "backendServers"{}
+# variable "frontendServers"{}
+# variable "backendServers"{}
 variable "dbServers"{}
-variable "publicServers"{}
+# variable "publicServers"{}
 variable "availability_zone"{}
 
 
