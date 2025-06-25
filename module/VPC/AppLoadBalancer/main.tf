f2a18797dfb2306baa86d14bfcc43893accb4dfb
Your Name <vemasani.pavani@gmail.com>
2025-06-25 18:08:09 +0530

    add gateway to public subnets

--- module/VPC/AppLoadBalancer/main.tf
+++ module/VPC/AppLoadBalancer/main.tf
@@ -73,7 +73,7 @@ resource "aws_route" "frontend_route" {
    count                     = length(var.publicServers)
    route_table_id            = aws_route_table.public[count.index].id
    destination_cidr_block    = var.default_vpc_cidr_block
-   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+   gateway_id                = aws_internet_gateway.igw.id
   }
 
    #  add internet gateway to a public Route
