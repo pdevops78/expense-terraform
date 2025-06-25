d341e782a77995417c10481c83f7b22a03b5905f
Your Name <vemasani.pavani@gmail.com>
2025-06-25 17:53:29 +0530

    add route to default route table

--- module/VPC/AppLoadBalancer/main.tf
+++ module/VPC/AppLoadBalancer/main.tf
@@ -42,3 +42,10 @@ resource "aws_route" "frontend_route" {
   destination_cidr_block    = var.default_vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
  }
+
+ #  add destination vpc cidr block to default route table id
+ resource "aws_route" "default_edit_route" {
+     route_table_id            = var.default_vpc_route_table_id
+     destination_cidr_block    = var.vpc_cidr_block
+     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+ }
