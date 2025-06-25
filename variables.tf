edb426706681d43f9e4f8b43fcf4095df443ce54
Your Name <vemasani.pavani@gmail.com>
2025-06-25 17:42:47 +0530

    declare a variable

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "default_vpc_id"{}
 variable "default_vpc_cidr_block"{}
 variable "default_vpc_route_table_id"{}
 variable "vpc_route_table_id"{}
-# variable "frontendServers"{}
+variable "frontendServers"{}
 # variable "backendServers"{}
 variable "dbServers"{}
 variable "publicServers"{}
