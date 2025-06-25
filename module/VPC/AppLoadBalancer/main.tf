301f221313fb5cfeba2495e346c78ae8f88b3d0f
Your Name <vemasani.pavani@gmail.com>
2025-06-25 18:00:17 +0530

    create public subnets

--- module/VPC/AppLoadBalancer/main.tf
+++ module/VPC/AppLoadBalancer/main.tf
@@ -49,6 +49,39 @@ resource "aws_route" "frontend_route" {
    subnet_id      = aws_subnet.frontend_subnets[count.index].id
    route_table_id = aws_route_table.frontend[count.index].id
  }
+#  create public subnets
+ resource "aws_subnet" "public_subnets" {
+   count       = length(var.publicServers)
+   vpc_id      = aws_vpc.vpc.id
+   cidr_block  = var.publicServers[count.index]
+   availability_zone = var.availability_zone[count.index]
+   tags = {
+     Name = "${var.env}-public-subnets-${count.index+1}"
+   }
+ }
+ #  create Route table for public subnets
+ resource "aws_route_table" "public" {
+   count   = length(var.publicServers)
+   vpc_id = aws_vpc.vpc.id
+   tags = {
+     Name = "${var.env}-public-route-${count.index+1}"
+       }
+   }
+
+ #  create a Route
+ resource "aws_route" "public_route" {
+   count                     = length(var.publicServers)
+   route_table_id            = aws_route_table.public[count.index].id
+   destination_cidr_block    = var.default_vpc_cidr_block
+   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+
+#  associate subnets with route table id
+resource "aws_route_table_association" "public" {
+  count          = length(var.publicServers)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
 
  #  add destination vpc cidr block to default route table id
  resource "aws_route" "default_edit_route" {
