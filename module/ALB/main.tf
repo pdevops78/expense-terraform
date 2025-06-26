6fcdc529fcbca4859dc9ae1f79ef27a5c3db7b14
Your Name <vemasani.pavani@gmail.com>
2025-06-26 09:00:47 +0530

    create listener

--- module/ALB/main.tf
+++ module/ALB/main.tf
@@ -25,7 +25,7 @@ resource "aws_lb_target_group_attachment" "tg_attach" {
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = "443"
+  port              = "80"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
