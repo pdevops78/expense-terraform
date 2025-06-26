a0112d11e0021671920c4f9e7f7cef741630d2e2
Your Name <vemasani.pavani@gmail.com>
2025-06-26 08:59:35 +0530

    create listener

--- module/ALB/main.tf
+++ module/ALB/main.tf
@@ -23,13 +23,14 @@ resource "aws_lb_target_group_attachment" "tg_attach" {
   port             = 80
 }
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.front_end.arn
   port              = "443"
   protocol          = "HTTP"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
+  }
   }
 
 # create a security group for Application Load Balancer
