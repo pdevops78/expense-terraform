8a6d49db3e773a1e9b2c8ee8219fbfe1068b14a5
Your Name <vemasani.pavani@gmail.com>
2025-06-28 12:36:01 +0530

    provisioner

--- module/app/data.tf
+++ module/app/data.tf
@@ -4,8 +4,8 @@ data "aws_ami" "ami" {
   owners           = [973714476881]
 }
 // this data will retrieve in json format
-data "vault_generic_secret" "get_secrets" {
-  path = "common/elastisearch"
-}
+# data "vault_generic_secret" "get_secrets" {
+#   path = "common/elastisearch"
+# }
 
 
