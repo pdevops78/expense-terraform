65fb7d05d665ae53d1c4af2fa9b50968aec0f7fb
Your Name <vemasani.pavani@gmail.com>
2025-06-28 13:06:21 +0530

    comment

--- module/app/variables.tf
+++ module/app/variables.tf
@@ -3,15 +3,15 @@ variable "component" {}
 variable "env" {}
 variable "zone_id"{}
 variable "vault_token"{}
-variable "vpc_id"{}
-variable "subnet_id"{}
+# variable "vpc_id"{}
+# variable "subnet_id"{}
 variable "lb_needed"{
 default = false
 }
 variable "lb_type"{
 default = null
 }
-variable "lb_subnets"{}
+# variable "lb_subnets"{}
 variable "server_app_port"{}
 variable "app_port"{}
 variable "lb_server_app_port"{}
