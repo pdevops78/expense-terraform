48b64d1014c8bad2a5b495d1761421978a5f351e
Your Name <vemasani.pavani@gmail.com>
2025-06-29 13:13:59 +0530

    db parameter name

--- module/app/main.tf
+++ module/app/main.tf
@@ -63,6 +63,7 @@ resource "null_resource" "provisioner" {
       "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/pdevops78/expense-ansible getsecrets.yml -e env=${var.env} -e component_name=${var.component} -e project_name=expense",
       "ansible-pull -i localhost, -U https://github.com/pdevops78/expense-ansible expense.yml -e env=${var.env} -e component_name=${var.component} -e @~/secrets.json"
+
     ]
   }
 
