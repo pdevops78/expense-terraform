26b81d920f1e54524ca6e6622ce27862ef687959
Your Name <vemasani.pavani@gmail.com>
2025-06-29 13:10:01 +0530

    input variable is missed

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "default_vpc_cidr_block"{}
 variable "default_vpc_route_table_id"{}
 variable "vpc_route_table_id"{}
 variable "frontendServers"{}
-# variable "backendServers"{}
+variable "backendServers"{}
 variable "dbServers"{}
 variable "publicServers"{}
 variable "availability_zone"{}
