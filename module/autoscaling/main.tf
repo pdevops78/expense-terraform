76ec514fd7e6bc802e23ef02d2c71cdc46ff54a5
Your Name <vemasani.pavani@gmail.com>
2025-07-01 14:18:06 +0530

    execute script file in launch template

--- module/autoscaling/main.tf
+++ module/autoscaling/main.tf
@@ -4,13 +4,13 @@ resource "aws_launch_template" "template" {
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids= [aws_security_group.sg.id]
-#  user_data = base64encode(
-#    templatefile("${path.module}/userdata.sh", {
-#      component = var.component
-#      env       = var.env
-#
-#    })
-#  )
+ user_data = base64encode(
+   templatefile("${path.module}/userdata.sh", {
+     component = var.component
+     env       = var.env
+
+   })
+ )
 
   tags = {
   Name = "${var.component}-${var.env}-lt"
