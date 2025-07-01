44f5edaed6d931b01f7923a40d178b4a3160cdab
Your Name <vemasani.pavani@gmail.com>
2025-07-01 13:21:00 +0530

    create backend subnets

--- main.tf
+++ main.tf
@@ -40,6 +40,44 @@ certificate_arn    = var.certificate_arn
 zone_id            = var.zone_id
 }
 
+module "backend"{
+source             = "./module/autoscaling"
+component          = "backend"
+env                = var.env
+instance_type      = var.instance_type
+subnet_id          = module.AppLoadBalancer.backend
+server_app_cidr    = concat(var.frontendServers,var.backendServers)
+app_port           = 8080
+vpc_id             = module.AppLoadBalancer.vpc_id
+bastion_node       = var.bastion_node
+lb_subnets         = module.AppLoadBalancer.backend
+lb_server_app_port = {http:8080}
+lb_server_app_cidr = var.backendServers
+lb_type            = "private"
+ssl_policy         = var.ssl_policy
+certificate_arn    = var.certificate_arn
+zone_id            = var.zone_id
+}
+
+# module "rds"{
+# source = "./module/rds"
+# component="mysql"
+# allocated_storage = 20
+# engine = "mysql"
+# engine_version = "8.0.36"
+# instance_class = "db.t3.micro"
+# storage_type = "gp3"
+# publicly_accessible = "no"
+# family = "mysql8.0"
+# multi_az = false
+# vpc_id = module.AppLoadBalancer.vpc_id
+# env=var.env
+# skip_final_snapshot = true
+# server_app_port = var.backendServers
+# subnet_id = module.AppLoadBalancer.db
+# kms_key_id = var.kms_key_id
+# }
+
 #  module "backend" {
 #    depends_on      = [module.mysql]
 #    source          = "./module/app"
